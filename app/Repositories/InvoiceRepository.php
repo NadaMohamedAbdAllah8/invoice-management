22ef9421aac8df959c43a7f29e774918d49f32b4
nadamohamedabdallah <nadamohamedabdallah8@gmail.com>
2026-02-25 19:03:16 +0200

    chore: Using toArray of DTO

--- app/Repositories/InvoiceRepository.php
+++ app/Repositories/InvoiceRepository.php
@@ -9,16 +9,7 @@ class InvoiceRepository implements InvoiceRepositoryInterface
 {
     public function createOne(CreateInvoiceDTO $data): Invoice
     {
-        return Invoice::create([
-            'contract_id' => $data->contract_id,
-            'invoice_number' => $data->invoice_number,
-            'subtotal' => $data->subtotal,
-            'tax_amount' => $data->tax_amount,
-            'total' => $data->total,
-            'status' => $data->status,
-            'due_date' => $data->due_date,
-            'paid_at' => $data->paid_at,
-        ]);
+        return Invoice::create($data->toArray());
     }
 
     public function countForUpdate(): int
