e3563ca2d28ee16e8ce868ee96a7308c8fae96f3
nadamohamedabdallah <nadamohamedabdallah8@gmail.com>
2026-02-26 19:17:04 +0200

    feat: View contract summary

--- app/Http/Resources/ContactInvoiceResource.php
+++ app/Http/Resources/ContactInvoiceResource.php
@@ -4,7 +4,7 @@ namespace App\Http\Resources;
 
 use Illuminate\Http\Resources\Json\JsonResource;
 
-class InvoiceFilterResource extends JsonResource
+class ContactInvoiceResource extends JsonResource
 {
     public function toArray($request): array
     {
