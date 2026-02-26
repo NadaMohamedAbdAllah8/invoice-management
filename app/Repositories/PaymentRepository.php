613b8fc7ea77032022c6f3f1acccb2ae7ac8a041
nadamohamedabdallah <nadamohamedabdallah8@gmail.com>
2026-02-26 14:52:37 +0200

    chore: Removing unused methods

--- app/Repositories/PaymentRepository.php
+++ app/Repositories/PaymentRepository.php
@@ -4,34 +4,11 @@ namespace App\Repositories;
 
 use App\Data\CreatePaymentDTO;
 use App\Models\Payment;
-use Illuminate\Database\Eloquent\Collection;
-
+=
 class PaymentRepository implements PaymentRepositoryInterface
 {
-    public function all(): Collection
-    {
-        return Payment::query()->get();
-    }
-
-    public function findById(int $id): ?Payment
-    {
-        return Payment::query()->find($id);
-    }
-
-    public function createOne(CreatePaymentDTO $dto): Payment
+      public function createOne(CreatePaymentDTO $dto): Payment
     {
         return Payment::create($dto->toArray());
-    }
-
-    public function update(Payment $payment, array $data): Payment
-    {
-        $payment->update($data);
-
-        return $payment->refresh();
-    }
-
-    public function delete(Payment $payment): bool
-    {
-        return (bool) $payment->delete();
     }
 }
--- app/Repositories/PaymentRepositoryInterface.php
+++ app/Repositories/PaymentRepositoryInterface.php
@@ -4,17 +4,8 @@ namespace App\Repositories;
 
 use App\Data\CreatePaymentDTO;
 use App\Models\Payment;
-use Illuminate\Database\Eloquent\Collection;
 
 interface PaymentRepositoryInterface
 {
-    public function all(): Collection;
-
-    public function findById(int $id): ?Payment;
-
     public function createOne(CreatePaymentDTO $dto): Payment;
-
-    public function update(Payment $payment, array $data): Payment;
-
-    public function delete(Payment $payment): bool;
 }
